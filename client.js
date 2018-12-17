bf0e4350f0a1f32250d4a832760cb251cf2fa110
Matt Kleven <klevennb115@gmail.com>
2018-12-16 18:01:48 -0600

    Started from scratch

--- client.js
+++ client.js
@@ -1,35 +1,102 @@
-$( document ).ready(function () {
-    console.log('in jquery');
-    
-    $('#submit-button').on('click', appendFunction);
-    // $('#submit-button').on('click', changeBudget);
-})
-
-function appendFunction() {
-    console.log('in appendFunction');
-    let firstName = $( '#first-name').val();  //takes input values
-    let lastName = $('#last-name').val();
-    let idNumber = $('#id-number').val();
-    let positionTitle = $('#position-title').val();
-    let annualSalary = $('#ann-salary').val();
-    $('table').append('<tr> <td>' + firstName + '</td> <td>' + lastName + '</td> <td>' + idNumber + '</td> + <td>' + positionTitle + '</td> + <td>' + annualSalary + '</td></tr>');  //appends to the table
-    $('.inputs').val(''); //emptys inputs
-    annualSalary = annualSalary / 12;  
-    console.log(annualSalary);
-    monthlyBudget = (monthlyBudget - annualSalary).toFixed(2);  //makes it only display 2 decimals
-    $('#total-monthly').empty().append('<h3>Total Monthly: $</h3>', monthlyBudget);
-    if (monthlyBudget < 0) {
-        $('#total-monthly').css('color','red');
+console.log('in js');
+class Employee {
+    constructor(firstName, lastName, idNumber, title, annualSal) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.title = title;
+        this.annualSal = annualSal;
     }
-}
+} //end Employee
+let employees = [];
 
-let monthlyBudget = 15000;
-// function changeBudget() {
-//     let annualSalary = $('#ann-salary').val();
-//     annualSalary = annualSalary/12;
-//     console.log(annualSalary);
-    
-//     monthlyBudget = monthlyBudget - annualSalary;
-//     $( '#total-monthly').empty().append( '<h3>Total Monthly: $</h3>', monthlyBudget);
+$(document).ready(function () {
+    console.log('in jquery');
+    $('#submit-button').on('click', pushToEmployees);
+}); //end .ready
 
+function pushToEmployees() {
+    console.log('in pushToEmployees');
+    let fName = $('#input-first').val();  //gathers inputs
+    let lName = $('#input-last').val();
+    let idNum = $('#input-id').val();
+    let arrTitle = $('#input-title').val();
+    let anSal = $('#input-annual').val();
+    let newEmp = new Employee(fName, lName, idNum, arrTitle, anSal);  //makes new Employee object
+    employees.push(newEmp);  //pushes to array
+    appendToTable();
+} //end pushToEmployees
+
+function appendToTable() {
+    $('table').empty(); //emptys table
+    $('#employee-table').append('<th>First Name</th> <th>Last Name</th> <th>Id Number</th> <th>Title</th> <th>Annual Salary</th>'); //adds table headers
+    for (let i = 0; i < employees.length; i++) {
+        $('#employee-table').append('<tr><td>' + employees[i].firstName + '</td><td>' + employees[i].lastName + '</td><td>' + employees[i].idNumber + '</td><td>' + employees[i].title + '</td><td>' + employees[i].annualSal + '</td></tr>') //adds input data
+    } //end for loop
+    deductFromBudget();
+} //end appendToTable
+
+function deductFromBudget() {
+
+
+} // end deductFromBudget
+
+
+
+// class Employee{
+//     constructor(firstName, lastName,idNumber, positionTitle, annualSalary){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.idNumber = idNumber;
+//         this.positionTitle = positionTitle;
+//         this.annualSalary = annualSalary;
+//     }
 // }
+
+// let employees =[];
+
+// $( document ).ready(function () {
+//     console.log('in jquery');
+    
+//     $('#submit-button').on('click', appendFunction);
+//     // $('#submit-button').on('click', changeBudget);
+// })
+// function collectInput(firstName, lastName, idNumber, positionTitle, annualSalary) {
+//     function addToEmployees(firstName, lastName, idNumber, positionTitle, annualSalary) {
+//         employees.push(new Employee(firstName, lastName, idNumber, positionTitle, annualSalary))
+//     } //end addToEmployees
+//     let empFirstName = $('#first-name').val();  //takes input values
+//     let empLastName = $('#last-name').val();
+//     let empIdNumber = $('#id-number').val();
+//     let empPositionTitle = $('#position-title').val();
+//     let empAnnualSalary = $('#ann-salary').val();
+//     addToEmployees(empFirstName, empLastName, empIdNumber, empPositionTitle, empAnnualSalary);
+   
+// } //end collectInput
+
+// function appendFunction() {
+//     console.log('in appendFunction');
+//     for (let i = 0; i < employees.length; i++) {
+//         const element = employees[i];
+//         $('table').append('<tr> <td>' + employees[i].firstName + '</td> <td>' + employees[i].lastName + '</td> <td>' + employees[i].idNumber + '</td> + <td>' + employees[i].positionTitle + '</td> + <td>' + employees[i].annualSalary + '</td></tr>');  //appends to the table
+//     }
+//     $('.inputs').val(''); //emptys inputs
+//     annualSalary = annualSalary / 12;  
+//     console.log(annualSalary);
+//     monthlyBudget = (monthlyBudget - annualSalary).toFixed(2);  //makes it only display 2 decimals
+//     $('#total-monthly').empty().append('<h3>Total Monthly: $</h3>', monthlyBudget);
+//     if (monthlyBudget < 0) {
+//         $('#total-monthly').css('color','red');
+//     }
+// }
+
+// let monthlyBudget = 15000;
+// // function changeBudget() {
+// //     let annualSalary = $('#ann-salary').val();
+// //     annualSalary = annualSalary/12;
+// //     console.log(annualSalary);
+    
+// //     monthlyBudget = monthlyBudget - annualSalary;
+// //     $( '#total-monthly').empty().append( '<h3>Total Monthly: $</h3>', monthlyBudget);
+
+// // }
